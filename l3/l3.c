6eab72058418a2f8b1c6ad1e026ae377657f303c
Charles Lindsay <chaz@chazomatic.us>
2014-03-24 22:39:15 -0700

    Flesh out image bits more

--- l3/l3.c
+++ l3/l3.c
@@ -16,21 +16,65 @@
 static lua_State *lua = NULL;
 
 
-static int image_new(lua_State *restrict l) {
-    return 0;
+static b3_rect get_rect(lua_State *restrict l, int index) {
+    lua_getfield(l, index, "x");
+    lua_getfield(l, index, "y");
+    lua_getfield(l, index, "width");
+    lua_getfield(l, index, "height");
+
+    int x_is_num, y_is_num, width_is_num, height_is_num;
+    int x = (int)lua_tointegerx(l, -4, &x_is_num);
+    int y = (int)lua_tointegerx(l, -3, &y_is_num);
+    int width = (int)lua_tointegerx(l, -2, &width_is_num);
+    int height = (int)lua_tointegerx(l, -1, &height_is_num);
+
+    if(!x_is_num || !y_is_num || !width_is_num || !height_is_num)
+        luaL_error(l, "rect requires integer x, y, width, and height");
+
+    lua_pop(l, 4);
+    return (b3_rect){x, y, width, height};
+}
+
+static b3_image **push_new_image(lua_State *restrict l) {
+    b3_image **p_image = lua_newuserdata(l, sizeof(*p_image));
+    luaL_getmetatable(l, IMAGE_METATABLE);
+    lua_setmetatable(l, -2);
+
+    return p_image;
+}
+
+static b3_image *check_image(lua_State *restrict l, int index) {
+    return *(b3_image **)luaL_checkudata(l, index, IMAGE_METATABLE);
+}
+
+static int image_load(lua_State *restrict l) {
+    const char *filename = luaL_checkstring(l, 1);
+    luaL_argcheck(l, filename && *filename, 1, "filename required");
+
+    b3_image **p_image = push_new_image(l);
+    *p_image = b3_load_image(filename);
+    return 1;
 }
 
 static int image_gc(lua_State *restrict l) {
+    b3_image *image = check_image(l, 1);
+    b3_free_image(image);
     return 0;
 }
 
 static int image_sub(lua_State *restrict l) {
-    return 0;
+    b3_image *image = check_image(l, 1);
+    luaL_checktype(l, 2, LUA_TTABLE);
+    b3_rect rect = get_rect(l, 2);
+
+    b3_image **p_image = push_new_image(l);
+    *p_image = b3_new_sub_image(image, &rect);
+    return 1;
 }
 
 static int open_image(lua_State *restrict l) {
     static const luaL_Reg functions[] = {
-        {"new", image_new},
+        {"load", image_load},
         {NULL, NULL}
     };
     static const luaL_Reg methods[] = {
