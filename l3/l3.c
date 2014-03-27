ae3d192c517ba0d162f65baf7e3506ce0888677a
Charles Lindsay <chaz@chazomatic.us>
2014-03-27 13:35:07 -0700

    Add map to l3

--- l3/l3.c
+++ l3/l3.c
@@ -9,14 +9,17 @@
 
 #define L3_NAME "l3"
 #define IMAGE_NAME "image"
+#define MAP_NAME "map"
 
 #define IMAGE_METATABLE L3_NAME "." IMAGE_NAME
+#define MAP_METATABLE L3_NAME "." MAP_NAME
 
 
 static lua_State *lua = NULL;
 
 
-static b3_rect get_rect_arg(lua_State *restrict l, int arg_index) {
+static b3_rect check_rect(lua_State *restrict l, int arg_index) {
+    luaL_checktype(l, arg_index, LUA_TTABLE);
     lua_getfield(l, arg_index, "x");
     lua_getfield(l, arg_index, "y");
     lua_getfield(l, arg_index, "width");
@@ -27,9 +30,12 @@ static b3_rect get_rect_arg(lua_State *restrict l, int arg_index) {
     int y = (int)lua_tointegerx(l, -3, &y_is_num);
     int width = (int)lua_tointegerx(l, -2, &width_is_num);
     int height = (int)lua_tointegerx(l, -1, &height_is_num);
-
-    if(!x_is_num || !y_is_num || !width_is_num || !height_is_num)
-        luaL_error(l, "rect requires integer x, y, width, and height");
+    luaL_argcheck(
+        l,
+        (x_is_num && y_is_num && width_is_num && height_is_num),
+        arg_index,
+        "rect requires integer x, y, width, and height fields"
+    );
 
     lua_pop(l, 4);
     return (b3_rect){x, y, width, height};
@@ -64,8 +70,7 @@ static int image_gc(lua_State *restrict l) {
 
 static int image_sub(lua_State *restrict l) {
     b3_image *image = check_image(l, 1);
-    luaL_checktype(l, 2, LUA_TTABLE);
-    b3_rect rect = get_rect_arg(l, 2);
+    b3_rect rect = check_rect(l, 2);
 
     b3_image **p_image = push_new_image(l);
     *p_image = b3_new_sub_image(image, &rect);
@@ -96,9 +101,100 @@ static int open_image(lua_State *restrict l) {
     return 1;
 }
 
+static b3_map *check_map(lua_State *restrict l, int index) {
+    return *(b3_map **)luaL_checkudata(l, index, MAP_METATABLE);
+}
+
+static int map_new(lua_State *restrict l) {
+    int width = (int)luaL_checkinteger(l, 1);
+    int height = (int)luaL_checkinteger(l, 2);
+
+    b3_map **p_map = lua_newuserdata(l, sizeof(*p_map));
+    luaL_getmetatable(l, MAP_METATABLE);
+    lua_setmetatable(l, -2);
+
+    *p_map = b3_new_map(width, height);
+    return 1;
+}
+
+static int map_gc(lua_State *restrict l) {
+    b3_map *map = check_map(l, 1);
+    b3_free_map(map);
+    return 0;
+}
+
+static int map_width(lua_State *restrict l) {
+    b3_map *map = check_map(l, 1);
+
+    lua_pushinteger(l, (lua_Integer)map->width);
+    return 1;
+}
+
+static int map_height(lua_State *restrict l) {
+    b3_map *map = check_map(l, 1);
+
+    lua_pushinteger(l, (lua_Integer)map->height);
+    return 1;
+}
+
+static int map_get_tile(lua_State *restrict l) {
+    b3_map *map = check_map(l, 1);
+    int x = (int)luaL_checkinteger(l, 2) - 1;
+    int y = (int)luaL_checkinteger(l, 3) - 1;
+
+    luaL_argcheck(l, x >= 0 && x < map->width, 2, "x must satisfy 1 <= x <= map:width()");
+    luaL_argcheck(l, y >= 0 && y < map->height, 3, "y must satisfy 1 <= y <= map:height()");
+
+    lua_pushinteger(l, (lua_Integer)B3_MAP_TILE(map, x, y));
+    return 1;
+}
+
+static int map_set_tile(lua_State *restrict l) {
+    b3_map *map = check_map(l, 1);
+    int x = (int)luaL_checkinteger(l, 2) - 1;
+    int y = (int)luaL_checkinteger(l, 3) - 1;
+    b3_tile tile = (b3_tile)luaL_checkinteger(l, 4);
+
+    luaL_argcheck(l, x >= 0 && x < map->width, 2, "x must satisfy 1 <= x <= map:width()");
+    luaL_argcheck(l, y >= 0 && y < map->height, 3, "y must satisfy 1 <= y <= map:height()");
+
+    B3_MAP_TILE(map, x, y) = tile;
+
+    lua_pushvalue(l, 1);
+    return 1;
+}
+
+static int open_map(lua_State *restrict l) {
+    static const luaL_Reg functions[] = {
+        {"new", map_new},
+        {NULL, NULL}
+    };
+    static const luaL_Reg methods[] = {
+        {"width", map_width},
+        {"height", map_height},
+        {"get_tile", map_get_tile},
+        {"set_tile", map_set_tile},
+        {NULL, NULL}
+    };
+
+    luaL_newmetatable(l, MAP_METATABLE);
+
+    lua_pushcfunction(l, map_gc);
+    lua_setfield(l, -2, "__gc");
+    lua_pushvalue(l, -1);
+    lua_setfield(l, -2, "__index");
+
+    luaL_setfuncs(l, methods, 0);
+    lua_pop(l, 1);
+
+    luaL_newlib(l, functions);
+    return 1;
+}
+
 static int open_all(lua_State *restrict l) {
     static const luaL_Reg submodules[] = {
         {IMAGE_NAME, open_image},
+        {MAP_NAME, open_map},
         {NULL, NULL}
     };
 
