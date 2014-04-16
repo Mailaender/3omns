aef3a074f5f22b4d799a96971c08022ec3330236
Charles Lindsay <chaz@chazomatic.us>
2014-04-16 12:16:48 -0700

    Never store "backing" entities in Lua

--- res/game/entities.lua
+++ res/game/entities.lua
@@ -28,16 +28,22 @@ function Entities:init(level)
   self.index = {}
 end
 
+-- The C entities are referred to as the "backing" for Lua entities.  Deal.
+function Entities:new_backing(entity)
+  local backing = self.level:new_entity(entity)
+  return backing:get_id(), backing
+end
+
+function Entities:get_backing(id)
+  return self.level:get_entity(id)
+end
+
 local function entities_index_key(pos)
   return string.format("%x,%x", pos.x, pos.y)
 end
 
 function Entities:get_entity(pos)
   return self.index[entities_index_key(pos)]
-end
-
-function Entities:new_entity(entity)
-  return self.level:new_entity(entity)
 end
 
 function Entities:move(entity, old_pos)
@@ -64,28 +70,37 @@ end
 
 function Entity:init(entities, pos, life, image)
   self.entities = entities
-  self.backing = entities:new_entity(self)
-  self:set_pos(pos)
-  self:set_life(life)
-  self:set_image(image)
+  local backing
+  self.id, backing = entities:new_backing(self)
+  self:set_pos(pos, backing)
+  self:set_life(life, backing)
+  self:set_image(image, backing)
 end
 
-function Entity:set_pos(pos)
+function Entity:get_backing()
+  return self.entities:get_backing(self.id)
+end
+
+function Entity:set_pos(pos, backing)
+  backing = backing or self:get_backing()
+
   if self.pos == nil or not pos_equal(pos, self.pos) then
     local old = self.pos
 
     self.pos = pos
-    self.backing:set_pos(pos)
+    backing:set_pos(pos)
 
     self.entities:move(self, old)
   end
   return self
 end
 
-function Entity:set_life(life)
+function Entity:set_life(life, backing)
+  backing = backing or self:get_backing()
+
   if life ~= self.life then
     self.life = life
-    self.backing:set_life(life)
+    backing:set_life(life)
 
     if life == 0 then
       self.entities:remove(self)
@@ -94,16 +109,18 @@ function Entity:set_life(life)
   return self
 end
 
-function Entity:set_image(image)
+function Entity:set_image(image, backing)
+  backing = backing or self:get_backing()
+
   if image ~= self.image then
     self.image = image
-    self.backing:set_image(image)
+    backing:set_image(image)
   end
   return self
 end
 
-function Entity:kill()
-  self:set_life(0)
+function Entity:kill(backing)
+  self:set_life(0, backing)
 end
 
 
