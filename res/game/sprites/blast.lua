67dc0cd18c3c51ac769d86af9038f335495831a7
Charles Lindsay <chaz@chazomatic.us>
2014-06-02 23:25:27 -0700

    Speckle blast animations

--- res/game/sprites/blast.lua
+++ res/game/sprites/blast.lua
@@ -4,40 +4,39 @@ local Sprite = require("sprites.sprite")
 local Blast = obj.class("Blast", Sprite)
 package.loaded[...] = Blast
 
-local util   = require("util")
+local util = require("util")
 
 
 Blast.TIME = 1
 
--- TODO: add some variance here, so the blasts aren't quite so uniform, but
--- speckle a bit as they dissipate.
-local function get_animation()
-  if not Blast.ANIMATION then
-    Blast.ANIMATION = {}
+function Blast:init_animation()
+  self.animation = {}
 
-    local blast_frame_duration = Blast.TIME / #IMAGES.BLASTS
-    for i, v in ipairs(IMAGES.BLASTS) do
-      Blast.ANIMATION[i] = {
-        time = Blast.TIME - (i - 1) * blast_frame_duration,
-        image = v,
-        z_order = #IMAGES.BLASTS - i,
-      }
+  local frame_duration = Blast.TIME / #IMAGES.BLASTS
+  for i, v in ipairs(IMAGES.BLASTS) do
+    local a = {
+      time = Blast.TIME - (i - 1) * frame_duration,
+      image = v,
+      z_order = #IMAGES.BLASTS - i,
+    }
+    if i > 1 then
+      a.time = a.time + math.random() * 0.05 - 0.025
     end
+    self.animation[i] = a
   end
-
-  return Blast.ANIMATION
 end
 
 function Blast:init(level, pos)
   local backing = Sprite.init(self, level, pos)
 
-  self.time = Blast.TIME
+  self.time = Blast.TIME - math.random() * 0.05
 
+  self:init_animation()
   self:animate(backing)
 end
 
 function Blast:animate(backing)
-  local frame = util.get_animation_frame(get_animation(), self.time)
+  local frame = util.get_animation_frame(self.animation, self.time)
   self:set_image(frame.image, backing)
   self:set_z_order(frame.z_order, backing)
 end
