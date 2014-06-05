14f9eba7dba086f983cb501d0995953d9063bcbd
Charles Lindsay <chaz@chazomatic.us>
2014-06-04 17:07:12 -0700

    GPL license

--- l3/internal.h
+++ l3/internal.h
@@ -2,6 +2,19 @@
     3omns - old-school arcade-style tile-based bomb-dropping deathmatch jam
     l3 - Lua interface library for 3omns
     Copyright 2014 Charles Lindsay <chaz@chazomatic.us>
+
+    3omns is free software: you can redistribute it and/or modify it under the
+    terms of the GNU General Public License as published by the Free Software
+    Foundation, either version 3 of the License, or (at your option) any later
+    version.
+
+    3omns is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+    FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+    details.
+
+    You should have received a copy of the GNU General Public License along
+    with 3omns.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #ifndef __internal_h__
