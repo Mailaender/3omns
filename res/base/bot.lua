582fc7bc21d27df015de5aa03c4b906649d3a223
Charles Lindsay <chaz@chazomatic.us>
2015-10-13 10:31:51 -0700

    Refactor bot code
    Instead of relying on tail call elimination, this sets up a loop that
    repeatedly calls the "do whatever we should be doing next" function.
    Then every action the bot can do simply returns the elapsed time of its
    combined sub-actions.  That way they're more easily composable, and the
    logic is easier to follow, too.

--- res/base/bot.lua
+++ res/base/bot.lua
@@ -36,128 +36,60 @@ end
 
 function Bot:co_start()
   -- Wait just a tick to be a little more human at the start.
-  self:co_wait(self.action_time + math.random(0, 5) / 10, 0)
+  self:co_wait(self.action_time + math.random(0, 5) / 10)
 
-  -- The AI system assumes proper tail-call elimination.  I believe I've
-  -- structured everything properly, but I haven't tested.
-  return self:co_rethink()
+  while true do
+    self:co_rethink()
+  end
 end
 
-function Bot:co_yield(elapsed)
+function Bot:co_rethink(danger)
+  danger = danger or self:get_danger()
+
+  if danger then
+    return self:co_run_away(danger)
+  end
+
+  -- TODO: go fishing for supers (or run toward an exposed one) when calm.
+  return self:co_hunt()
+end
+
+function Bot:co_yield()
   coroutine.yield()
-  return elapsed + self.yield_time
+  return self.yield_time
 end
 
-function Bot:co_wait(till, elapsed)
-  while elapsed < till do
-    elapsed = self:co_yield(elapsed)
+function Bot:do_until(act, done)
+  local elapsed = 0
+  while not done(elapsed) do
+    elapsed = elapsed + act()
   end
   return elapsed
 end
+
+function Bot:co_wait(duration)
+  return self:do_until(
+    function() return self:co_yield() end,
+    function(elapsed) return elapsed >= duration end
+  )
+end
+
+function Bot:co_act(action)
+  self.dude:l3_action(self.dude_backing, action)
+
+  return self:co_wait(self.action_time)
+end
+
+function Bot:co_fire()
+  return self:co_act("f")
+end
+
+Bot.co_move = Bot.co_act -- Assume action is u/d/l/r, but it's w/e.
 
 function Bot:get_nearest_dudes(pos)
   local dudes = self.dude:get_nearest(Entities.Dude, pos)
   table.remove(dudes, 1) -- Nix self.
   return dudes
-end
-
-function Bot:co_random_walk(interrupt)
-  interrupt = interrupt or function() return false end
-
-  local elapsed = 0
-  local next_action = 0
-
-  while not interrupt(elapsed) do
-    if elapsed >= next_action then
-      next_action = next_action + self.action_time
-
-      local dirs = {"u", "d", "l", "r"}
-      self.dude:move(dirs[math.random(#dirs)], self.dude_backing)
-    end
-
-    elapsed = self:co_yield(elapsed)
-  end
-
-  self:co_wait(next_action, elapsed)
-end
-
-function Bot:co_move_to(pos, interrupt)
-  interrupt = interrupt or function() return false end
-
-  local elapsed = 0
-  local next_action = 0
-  local path = {}
-
-  util.line(self.dude.pos, pos, function(p)
-    if self.dude.entities:walkable(p) then
-      path[#path + 1] = p
-    end
-    return not core.pos_equal(p, pos)
-  end)
-  table.remove(path, 1) -- Nix current pos.
-
-  while #path > 0 and not interrupt(elapsed) do
-    if elapsed >= next_action then
-      next_action = next_action + self.action_time
-
-      -- TODO: proper pathfinding that avoids dangers and obstacles.
-      local m = {}
-      local function add_if_valid(dir)
-        if self.dude.entities:walkable(self.dude:move_pos(dir)) then
-          m[#m + 1] = dir
-        end
-      end
-      if     path[1].y < self.dude.pos.y then add_if_valid("u")
-      elseif path[1].y > self.dude.pos.y then add_if_valid("d") end
-      if     path[1].x < self.dude.pos.x then add_if_valid("l")
-      elseif path[1].x > self.dude.pos.x then add_if_valid("r") end
-
-      if #m > 1 then
-        local dx = math.abs(path[1].x - self.dude.pos.x)
-        local dy = math.abs(path[1].y - self.dude.pos.y)
-        if dx > dy then
-          -- TODO: using underscore.lua would help here.
-          local t = {}
-          for _, v in ipairs(m) do
-            if v ~= "u" and v ~= "d" then t[#t + 1] = v end
-          end
-          m = t
-        elseif dy > dx then
-          local t = {}
-          for _, v in ipairs(m) do
-            if v ~= "l" and v ~= "r" then t[#t + 1] = v end
-          end
-          m = t
-        end
-      end
-
-      if #m == 0 then
-        if path[1].x ~= self.dude.pos.x then
-          add_if_valid("u")
-          add_if_valid("d")
-        else
-          add_if_valid("l")
-          add_if_valid("r")
-        end
-      end
-
-      self.dude:move(m[math.random(#m)], self.dude_backing)
-
-      if core.pos_equal(self.dude.pos, path[1]) then
-        table.remove(path, 1)
-      end
-    end
-
-    elapsed = self:co_yield(elapsed)
-  end
-
-  self:co_wait(next_action, elapsed)
-end
-
-function Bot:co_fire()
-  self.dude:fire(self.dude_backing)
-
-  self:co_wait(self.action_time, 0)
 end
 
 function Bot:get_danger(pos)
@@ -181,16 +113,92 @@ function Bot:get_danger(pos)
   return nil
 end
 
--- TODO: go fishing for supers (or run toward an exposed one) when calm.
+function Bot:co_random_walk(interrupt)
+  local actions = {"u", "d", "l", "r"}
+  return self:do_until(function()
+    return self:co_move(actions[math.random(#actions)])
+  end, interrupt)
+end
 
-function Bot:co_rethink(danger)
-  danger = danger or self:get_danger()
+function Bot:co_move_to(pos, interrupt)
+  local path = {}
+  util.line(self.dude.pos, pos, function(p)
+    if self.dude.entities:walkable(p) then
+      path[#path + 1] = p
+    end
+    return not core.pos_equal(p, pos)
+  end)
+  table.remove(path, 1) -- Nix current pos.
 
-  if danger then
-    return self:co_run_away(danger)
-  else
-    return self:co_hunt()
+  -- TODO: proper pathfinding that avoids dangers and obstacles.
+  local function get_valid_actions_for_next_step()
+    local valid_actions = {}
+    local function add_action_if_valid(action)
+      if self.dude.entities:walkable(self.dude:move_pos(action)) then
+        valid_actions[#valid_actions + 1] = action
+      end
+    end
+    if     path[1].y < self.dude.pos.y then add_action_if_valid("u")
+    elseif path[1].y > self.dude.pos.y then add_action_if_valid("d") end
+    if     path[1].x < self.dude.pos.x then add_action_if_valid("l")
+    elseif path[1].x > self.dude.pos.x then add_action_if_valid("r") end
+
+    -- In case of a non-walkable obstacle, there will be a discontinuity in
+    -- path.  This makes sure we go around it the most efficient way.
+    if #valid_actions > 1 then
+      local dx = math.abs(path[1].x - self.dude.pos.x)
+      local dy = math.abs(path[1].y - self.dude.pos.y)
+      if dx > dy then
+        -- TODO: using underscore.lua would help here.
+        local t = {}
+        for _, v in ipairs(valid_actions) do
+          if v ~= "u" and v ~= "d" then t[#t + 1] = v end
+        end
+        valid_actions = t
+      elseif dy > dx then
+        local t = {}
+        for _, v in ipairs(valid_actions) do
+          if v ~= "l" and v ~= "r" then t[#t + 1] = v end
+        end
+        valid_actions = t
+      end
+    end
+
+    -- In case of an obstacle directly in our path, move around it any way
+    -- possible.
+    if #valid_actions == 0 then
+      if path[1].x ~= self.dude.pos.x then
+        add_action_if_valid("u")
+        add_action_if_valid("d")
+      else
+        add_action_if_valid("l")
+        add_action_if_valid("r")
+      end
+    end
+
+    return valid_actions
   end
+
+  local function co_step()
+    -- TODO: need a contingency plan for no valid_actions.  This could happen
+    -- if there were e.g. one wall to our right and one below us, and the next
+    -- path point was down and to the right.  It won't happen with the current
+    -- way levels are generated, but it's a possibility in general.  See also:
+    -- need proper pathfinding.
+    local valid_actions = get_valid_actions_for_next_step()
+    local elapsed = self:co_move(valid_actions[math.random(#valid_actions)])
+
+    if core.pos_equal(self.dude.pos, path[1]) then
+      table.remove(path, 1)
+    end
+
+    return elapsed
+  end
+
+  return self:do_until(
+    co_step,
+    function(elapsed) return #path == 0 or interrupt(elapsed) end
+  )
 end
 
 function Bot:co_run_away(danger)
@@ -225,19 +233,18 @@ function Bot:co_run_away(danger)
     util.line(self.dude.pos, core.pos_add(self.dude.pos, d), safe_callback)
   end
   table.sort(safe, function(a, b) return a.dist < b.dist end)
+  -- TODO: omit paths (unless it's the only one) that go through a dude.
 
-  local function interrupt_walk(elapsed)
+  local function interrupt(elapsed)
     return elapsed > 3
   end
 
-  if #safe > 0 then
-    self:co_move_to(safe[1].pos, interrupt_walk)
-  else
+  if #safe == 0 then
     -- TODO: there should always be a safe place.
-    self:co_random_walk(interrupt_walk)
+    return self:co_random_walk(interrupt)
   end
 
-  return self:co_rethink()
+  return self:co_move_to(safe[1].pos, interrupt)
 end
 
 function Bot:co_hunt()
@@ -251,40 +258,37 @@ function Bot:co_hunt()
     end
   end
 
-  local function interrupt_walk(elapsed)
+  local function interrupt(elapsed)
     return elapsed > 1 or self:get_danger()
   end
 
-  if target then
-    local target_cornered = false
-    if self.dude:can_fire() then
-      local s = self.dude.entities.level_size
-      local corners = {
-        core.Pos(1,       1),
-        core.Pos(s.width, 1),
-        core.Pos(s.width, s.height),
-        core.Pos(1,       s.height),
-      }
-
-      for _, c in ipairs(corners) do
-        local dist = core.blast_dist(self.dude.pos, c)
-        if dist <= Entities.Bomn.RADIUS
-            and core.blast_dist(target.pos, c) < dist then
-          target_cornered = true
-          break
-        end
-      end
-    end
-
-    if target_cornered then
-      self:co_fire()
-    else
-      self:co_move_to(target.pos, interrupt_walk)
-    end
-  else
-    -- TODO: a better idle loop.
-    self:co_random_walk(interrupt_walk)
+  if not target then
+    return self:co_random_walk(interrupt)
   end
 
-  return self:co_rethink()
+  local target_cornered = false
+  if self.dude:can_fire() then
+    local s = self.dude.entities.level_size
+    local corners = {
+      core.Pos(1,       1),
+      core.Pos(s.width, 1),
+      core.Pos(s.width, s.height),
+      core.Pos(1,       s.height),
+    }
+
+    for _, c in ipairs(corners) do
+      local dist = core.blast_dist(self.dude.pos, c)
+      if dist <= Entities.Bomn.RADIUS
+          and core.blast_dist(target.pos, c) < dist then
+        target_cornered = true
+        break
+      end
+    end
+  end
+
+  if target_cornered then
+    return self:co_fire()
+  end
+
+  return self:co_move_to(target.pos, interrupt)
 end
