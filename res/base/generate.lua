2a283969aafc6043b08a825fdd65f31fba99408b
Charles Lindsay <chaz@chazomatic.us>
2015-10-16 22:37:31 -0700

    Refactor level generation
    This takes advantage of lua language features a little more nicely.

--- res/base/generate.lua
+++ res/base/generate.lua
@@ -20,6 +20,7 @@
 local gen = {}
 package.loaded[...] = gen
 
+local obj = require("object")
 local core = require("core")
 local util = require("util")
 local Entities = require("entities")
@@ -28,7 +29,32 @@ local Entities = require("entities")
 local MAP_SIZE = core.Size(30, 30)
 local MAX_ENTITIES = MAP_SIZE.width * MAP_SIZE.height + 4
 
-local function generate_spawns(ctx)
+local Generator = obj.class("Generator")
+
+function gen.generate()
+  local level = l3.level.new(MAP_SIZE, MAX_ENTITIES)
+  Generator(level, Entities(level)):generate()
+  return level
+end
+
+function Generator:init(level, entities)
+  self.level = level
+  self.entities = entities
+  self.spawns = {}
+  self.dudes = {}
+  self.crates = {}
+end
+
+function Generator:generate()
+  self:set_spawns()
+  self:add_walls()
+  self:fill_space()
+  self:spawn_dudes()
+  self:spawn_crates()
+  self:fill_crates()
+end
+
+function Generator:set_spawns()
   local quads = {
     core.Pos(MAP_SIZE.width / 2, MAP_SIZE.height / 2),
     core.Pos(0, 0),
@@ -37,40 +63,21 @@ local function generate_spawns(ctx)
   }
 
   for i, q in ipairs(quads) do
-    ctx.spawns[i] = core.Pos(
+    self.spawns[i] = core.Pos(
       math.random(q.x + 2, q.x + MAP_SIZE.width  / 2 - 2),
       math.random(q.y + 2, q.y + MAP_SIZE.height / 2 - 2)
     )
   end
 end
 
-local function spawn_at(ctx, pos)
-  for _, s in ipairs(ctx.spawns) do
+function Generator:spawn_at(pos)
+  for _, s in ipairs(self.spawns) do
     if core.pos_equal(pos, s) then return true end
   end
   return false
 end
 
-local function wall_grid(ctx, center)
-  local walls = {}
-  for x = center.x - 3, center.x + 3, 3 do
-    for y = center.y - 3, center.y + 3, 3 do
-      if not spawn_at(ctx, core.Pos(x, y)) then
-        walls[#walls + 1] = core.Pos(x, y)
-      end
-    end
-  end
-
-  while #walls > 5 do
-    table.remove(walls, math.random(#walls))
-  end
-
-  for _, w in ipairs(walls) do
-    ctx.level:set_tile(w, TILES.WALL)
-  end
-end
-
-local function generate_walls(ctx)
+function Generator:add_walls()
   local fourth = core.Size(
     math.floor(MAP_SIZE.width  / 4),
     math.floor(MAP_SIZE.height / 4)
@@ -82,50 +89,64 @@ local function generate_walls(ctx)
     core.Pos(MAP_SIZE.width - fourth.width, MAP_SIZE.height - fourth.height),
   }
 
-  for _, p in ipairs(pos) do
-    wall_grid(ctx, p)
+  local function wall_grid(center)
+    local walls = {}
+    for x = center.x - 3, center.x + 3, 3 do
+      for y = center.y - 3, center.y + 3, 3 do
+        if not self:spawn_at(core.Pos(x, y)) then
+          walls[#walls + 1] = core.Pos(x, y)
+        end
+      end
+    end
+
+    while #walls > 5 do
+      table.remove(walls, math.random(#walls))
+    end
+
+    for _, w in ipairs(walls) do
+      self.level:set_tile(w, TILES.WALL)
+    end
   end
-  wall_grid(ctx, core.Pos(MAP_SIZE.width / 2, MAP_SIZE.height / 2))
+
+  for _, p in ipairs(pos) do
+    wall_grid(p)
+  end
+  wall_grid(core.Pos(MAP_SIZE.width / 2, MAP_SIZE.height / 2))
 end
 
-local function fill_space(ctx)
+function Generator:fill_space()
   for x = 1, MAP_SIZE.width do
     for y = 1, MAP_SIZE.height do
-      if ctx.level:get_tile(core.Pos(x, y)) == 0 then
-        ctx.level:set_tile(core.Pos(x, y), TILES.BLANK)
+      if self.level:get_tile(core.Pos(x, y)) == 0 then
+        self.level:set_tile(core.Pos(x, y), TILES.BLANK)
       end
     end
   end
 end
 
-local function spawn_dudes(ctx)
-  for i, s in ipairs(ctx.spawns) do
-    ctx.dudes[i] = ctx.entities:Dude(s, i)
+function Generator:spawn_dudes()
+  for i, s in ipairs(self.spawns) do
+    self.dudes[i] = self.entities:Dude(s, i)
   end
 end
 
-local function valid(pos)
-  return pos.x >= 1 and pos.x <= MAP_SIZE.width
-      and pos.y >= 1 and pos.y <= MAP_SIZE.height
+function Generator:is_empty(pos)
+  return self.level:get_tile(pos) ~= TILES.WALL
+      and not self.entities:get_entity(pos)
 end
 
-local function empty(ctx, pos)
-  return ctx.level:get_tile(pos) ~= TILES.WALL
-      and not ctx.entities:get_entity(pos)
-end
+function Generator:spawn_crates()
+  local function bisect(a, b)
+    -- Rounded up or down at random.
+    return core.Pos(
+      math.floor((a.x + b.x) / 2 + 0.5 * math.random(0, 1)),
+      math.floor((a.y + b.y) / 2 + 0.5 * math.random(0, 1))
+    )
+  end
 
-local function bisect(a, b)
-  -- Rounded up or down at random.
-  return core.Pos(
-    math.floor((a.x + b.x) / 2 + 0.5 * math.random(0, 1)),
-    math.floor((a.y + b.y) / 2 + 0.5 * math.random(0, 1))
-  )
-end
-
-local function spawn_crates(ctx)
   local span_dir
   local function crates(pos)
-    if not valid(pos) then return false end
+    if not self.entities:pos_valid(pos) then return false end
 
     local start = core.Pos(
       pos.x + span_dir.x * math.random(-2, -1),
@@ -134,8 +155,8 @@ local function spawn_crates(ctx)
 
     for i = 0, 3 do
       local p = core.Pos(start.x + span_dir.x * i, start.y + span_dir.y * i)
-      if valid(p) and empty(ctx, p) then
-        ctx.crates[#ctx.crates + 1] = ctx.entities:Crate(p)
+      if self.entities:pos_valid(p) and self:is_empty(p) then
+        self.crates[#self.crates + 1] = self.entities:Crate(p)
       end
     end
 
@@ -143,52 +164,27 @@ local function spawn_crates(ctx)
   end
 
   local center = core.Pos(MAP_SIZE.width / 2, MAP_SIZE.height / 2)
-  local bisection
 
-  bisection = bisect(ctx.spawns[1], ctx.spawns[3])
   span_dir = core.Pos(0, 1)
-  util.line(center, bisection, crates)
+  util.line(center, bisect(self.spawns[1], self.spawns[3]), crates)
 
-  bisection = bisect(ctx.spawns[2], ctx.spawns[4])
   span_dir = core.Pos(0, 1)
-  util.line(center, bisection, crates)
+  util.line(center, bisect(self.spawns[2], self.spawns[4]), crates)
 
-  bisection = bisect(ctx.spawns[2], ctx.spawns[3])
   span_dir = core.Pos(1, 0)
-  util.line(center, bisection, crates)
+  util.line(center, bisect(self.spawns[2], self.spawns[3]), crates)
 
-  bisection = bisect(ctx.spawns[1], ctx.spawns[4])
   span_dir = core.Pos(1, 0)
-  util.line(center, bisection, crates)
+  util.line(center, bisect(self.spawns[1], self.spawns[4]), crates)
 end
 
-local function fill_crates(ctx)
-  for i = 1, #ctx.spawns do
+function Generator:fill_crates()
+  for i = 1, #self.spawns do
     local c
     repeat
-      c = ctx.crates[math.random(#ctx.crates)]
+      c = self.crates[math.random(#self.crates)]
     until not c:holds()
 
-    c:hold(ctx.entities.Super)
+    c:hold(self.entities.Super)
   end
-end
-
-function gen.generate()
-  local level = l3.level.new(MAP_SIZE, MAX_ENTITIES)
-  local ctx = {
-    level = level,
-    entities = Entities(level),
-    spawns = {},
-    dudes = {},
-    crates = {},
-  }
-
-  generate_spawns(ctx)
-  generate_walls(ctx)
-  fill_space(ctx)
-  spawn_dudes(ctx)
-  spawn_crates(ctx)
-  fill_crates(ctx)
-
-  return level
 end
